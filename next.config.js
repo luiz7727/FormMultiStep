203e84b8ccc92cbcdcd4d59955eb7e6397c636bc
luiz7727 <luiz.antonio999@hotmail.com>
2023-07-11 12:58:09 -0300

    project done

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'i.imgur.com',
+      },
+    ],
+  },
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,155 @@
-import Image from 'next/image'
+"use client"
+
+import { useState } from "react";
+import { number, z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
+import 'animate.css';
+
+const mySchema = z.object({
+  name: z.string().min(4,{ message:"Name is required" }),
+  telephone:z.string(),
+  email:z.string(),
+  companyName:z.string(),
+  employeeNumber:z.number(),
+  aboutBusiness:z.string(),
+  projectGoal:z.string()
+}).required();
+
+
+interface DataForm {
+  name:string,
+  telephone:string,
+  email:string,
+  companyName:string,
+  employeeNumber:number,
+  aboutBusiness:string,
+  projectGoal:string
+}
 
 export default function Home() {
+
+  const [step,setStep] = useState<number>(1);
+
+  const { register, watch, handleSubmit,formState: { errors } } = useForm<DataForm>({
+    resolver: zodResolver(mySchema)
+  })
+
+  console.log(errors);
+  function onSubmit(data:DataForm) {
+   console.log(data);
+  }
+
+  function validateBeforeGoToNextStep(numberStep:number) {
+    if(numberStep === 1 && watch("name").length > 0 && watch("telephone").length > 0 && watch("email").length > 0) {
+      setStep(step + 1);
+    }
+
+    if(numberStep === 2 && watch("companyName").length > 0 && watch("employeeNumber") && watch("aboutBusiness").length > 0) {
+      setStep(step + 1);
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="w-screen h-screen flex items-center justify-center bg-white text-base p-2">
+
+      <section className="w-[37rem] h-auto flex flex-col border-2 rounded-sm p-4 gap-1">
+        
+        <div className="w-full flex items-center justify-between rounded-sm">
+
+          <div className="flex items-center">
+            <div className={`w-7 h-7 p-2 flex items-center justify-center rounded-full bg-[#E1E1E6] ${step === 1 && 'bg-violet-900'}`}>
+              <p className={`text-[#8D8D99] ${step === 1 && 'text-white'}`}>1</p>
+            </div>
+            <p className={`ml-2 text-[#8D8D99] ${step === 1 && 'text-black'}`}>Contato</p>
+          </div>
+
+          <Image width={8} height={8} className="w-auto h-auto" src="https://i.imgur.com/9S7IJxm.png" alt=""/>
+
+          <div className="flex items-center">
+            <div className={`w-7 h-7 p-2 flex items-center justify-center rounded-full bg-[#E1E1E6] ${step === 2 && 'bg-violet-900'}`}>
+              <p className={`text-[#8D8D99] ${step === 2 && 'text-white'}`}>2</p>
+            </div>
+            <p className={`ml-2 text-[#8D8D99] ${step === 2 && 'text-black'}`}>Empresa</p>
+          </div>
+
+          <Image width={8} height={8} className="w-auto h-auto" src="https://i.imgur.com/9S7IJxm.png" alt=""/>
+
+          <div className="flex items-center">
+            <div className={`w-7 h-7 p-2 flex items-center justify-center rounded-full bg-[#E1E1E6] ${step === 3 && 'bg-violet-900'}`}>
+              <p className={`text-[#8D8D99] ${step === 3 && 'text-white'}`}>3</p>
+            </div>
+            <p className={`ml-2 text-[#8D8D99] ${step === 3 && 'text-black'}`}>Projeto</p>
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className="w-full h-px border-2 my-8"/>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <form className="w-full flex items-start flex-col" onSubmit={handleSubmit(onSubmit)}>
+          {
+            step === 1 && (
+              <div className="w-full flex flex-col gap-4 ">
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                <div className="w-full flex flex-col">
+                  <label className="text-black">Nome</label>
+                  <input autoComplete="off" {...register("name")} type="text" className="p-2 border-2 border-[#E1E1E6] rounded-sm text-black outline-none" placeholder="Como prefere ser chamado"/>
+                </div>
+                <div className="w-full flex flex-col">
+                  <label className="text-black">Telefone</label>
+                  <input autoComplete="off" { ...register("telephone") } type="text" className="p-2 border-2 border-[#E1E1E6] text-black outline-none" placeholder="Digite seu número de WhatsApp"/>
+                </div>
+                <div className="w-full flex flex-col">
+                  <label className="text-black">E-mail</label>
+                  <input autoComplete="off" {...register("email")} type="text" className="p-2 border-2 border-[#E1E1E6] text-black outline-none" placeholder="Digite seu e-mail"/>
+                </div>
+                <div className="w-full flex items-center justify-between mt-8">
+                  <div/>
+                  <button className="p-2 rounded-sm bg-[#633BBC] text-white uppercase" onClick={() => validateBeforeGoToNextStep(step)}>Continuar</button>
+                </div>
+              </div>
+            )
+          }
+          {
+            step === 2 && (
+              <div className="w-full flex flex-col gap-4 ">
+                <div className="w-full flex flex-col">
+                  <label className="text-black">Nome da empresa</label>
+                  <input autoComplete="off" { ...register("companyName") } type="text" className="p-2 border-2 border-[#E1E1E6] rounded-sm text-black outline-none" placeholder="Qual é o nome da empresa"/>
+                </div>
+                <div className="w-full flex flex-col">
+                  <label className="text-black">Número de funcionários</label>
+                  <input autoComplete="off" {...register("employeeNumber", {valueAsNumber:true})} type="number" className="p-2 border-2 border-[#E1E1E6] text-black outline-none" placeholder="Digite o número de colaboradores"/>
+                </div>
+                <div className="w-full flex flex-col">
+                  <label className="text-black">Sobre seu negócio</label>
+                  <input autoComplete="off" {...register("aboutBusiness")} type="text" className="p-2 border-2 border-[#E1E1E6] text-black outline-none" placeholder="Fale um pouco sobre seus produtos ou serviços"/>
+                </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+                <div className="w-full flex items-center justify-between mt-8">
+                  <button className="uppercase w-36 text-[#633BBC] border-2 border-[#633BBC] bg-white rounded-sm p-2" onClick={() => setStep(step - 1)}>Voltar</button>
+                  <button className="p-2 rounded-sm bg-[#633BBC] text-white uppercase" onClick={() => validateBeforeGoToNextStep(step)}>Continuar</button>
+                </div>
+              </div>
+            )
+          }
+          {
+            step === 3 && (
+              <div className="w-full flex flex-col gap-4 ">
+                <div className="w-full flex flex-col ">
+                  <label className="text-black">Objetivo do projeto</label>
+                  <textarea {...register("projectGoal")} className="h-24 border-2 p-2 text-black outline-none" placeholder="Descreva quais os objetivos desse projeto"></textarea>
+                </div>
+                <div className="w-full flex items-center justify-between mt-8">
+                  <button className="uppercase w-36 text-[#633BBC] border-2 border-[#633BBC] bg-white rounded-sm p-2" onClick={() => setStep(step - 1)}>Voltar</button>
+                  <button type="submit" className="p-2 rounded-sm bg-[#633BBC] text-white uppercase">Enviar Proposta</button>
+                </div>
+              </div>
+            )
+          }
+        </form>
+      </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
